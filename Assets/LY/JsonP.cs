08acbc022bd4db51c37a0948df340bfca1b3e4c5
刘岩 <362504741@qq.com>
2018-08-06 16:36:04 +0800

    添加玩家类,物品类和装备类

---
+++ Assets/LY/JsonP.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+
+namespace Common{
+	public class JsonP {
+
+		//保存玩家数据
+		public void SavePlayer(Player p){
+			string filePath = Application.dataPath + @"/Resources/JsonPlayer.json";
+			//找到当前路径
+			FileInfo file = new FileInfo(filePath);
+			//判断有没有文件，有则打开文件，，没有创建后打开文件
+			StreamWriter sw = file.CreateText();
+			//ToJson接口将你的列表类传进去，，并自动转换为string类型
+			string json = JsonMapper.ToJson(p);
+			//将转换好的字符串存进文件，
+			sw.WriteLine(json);
+			sw.Close();
+			sw.Dispose();
+		}
+
+		//读取玩家数据
+		public void LoadPlayer()
+		{
+			//调试用的  //Debug.Log(1);
+
+			//TextAsset该类是用来读取配置文件的
+			TextAsset asset = Resources.Load("JsonPlayer") as TextAsset;
+			if (!asset)  //读不到就退出此方法
+				return;
+
+			string strdata = asset.text;        
+			JsonData jsdata = JsonMapper.ToObject(strdata);
+			//在这里循环输出表示读到了数据，，即此数据可以使用了
+			for (int i = 0; i < jsdata.Count; i++)
+			{
+				Debug.Log(jsdata[i]);
+			}
+
+
+			Player.Instance.p_id = jsdata["p_id"].ToString();
+			Player.Instance.p_imagePath = jsdata["p_imagePath"].ToString();
+			Player.Instance.p_nickname = jsdata ["p_nickname"].ToString();
+			Player.Instance.p_description = jsdata ["p_description"].ToString();
+
+
+			Player.Instance.energy = int.Parse( jsdata ["engergy"].ToString());
+			Player.Instance.conenergy_rate = int.Parse( jsdata ["conenergy_rate"].ToString());
+			Player.Instance.collect_level = int.Parse( jsdata ["collect_level"].ToString());
+			Player.Instance.hunting_level =int.Parse( jsdata ["hunting_level"].ToString());
+			Player.Instance.logging_level = int.Parse( jsdata ["logging_level"].ToString());
+			Player.Instance.dig_level = int.Parse( jsdata ["dig_level"].ToString());
+
+
+			Player.Instance.fish_level = int.Parse( jsdata ["fish_level"].ToString());
+			Player.Instance.mk_china_lv = int.Parse( jsdata ["mk_china_lv"].ToString());
+			Player.Instance.mk_build_lv = int.Parse( jsdata ["mk_build_lv"].ToString());
+			Player.Instance.mk_weapon_lv = int.Parse( jsdata ["mk_weapon_lv"].ToString());
+
+
+			Player.Instance.p_head_id = int.Parse( jsdata ["p_head_id"].ToString());
+			Player.Instance.p_foot_id = int.Parse( jsdata ["p_foot_id"].ToString());
+			Player.Instance.p_body_id = int.Parse( jsdata ["p_body_id"].ToString());
+			Player.Instance.p_trousers_id = int.Parse( jsdata ["p_trousers_id"].ToString());
+		}
+
+
+	} 
+}
---
+++ Assets/LY/Player.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPropertyAttribute : PropertyAttribute
+{
+	public string Name { get; private set; }
+	public bool IsDirty { get; set; }
+
+	public SetPropertyAttribute(string name)
+	{
+		this.Name = name;
+	}
+}
+
+public class Player : Singleton<Player> {
+
+	//id
+	public	string p_id;
+
+	//图片路径
+	public string p_imagePath;
+
+	//名字
+	public string p_nickname;
+
+	//描述
+	public string p_description;
+
+	[SerializeField,SetPropertyAttribute("hp")]
+	private int _energy;
+	public int energy{
+		set{
+			_energy = value;
+			if (_energy > 100)
+				_energy = 100;
+		}
+		get{ 
+			if (_energy < 0)
+				_energy = 0;
+			return _energy;
+		}
+	}  
+
+	public int conenergy_rate;
+
+
+
+
+	//战斗力
+	[SerializeField,SetPropertyAttribute("power")]
+	private int _power;
+	public int power{
+		set{ _power = value;}
+		get{ return _power;}
+	}
+
+	// 采集相关
+
+	//采集等级
+	private int _collect_level;
+	public int collect_level{set{ _collect_level = value;} get{ return _collect_level;}}
+
+	// 狩猎等级
+	private int _hunting_level;
+	public int hunting_level{set{ _hunting_level = value;}get{ return _hunting_level;}}
+
+
+	// 伐木等级
+	private int _logging_level;
+	public int logging_level{set{ _logging_level = value;}get{ return _logging_level;}}
+
+	// 挖矿等级
+	private int _dig_level;
+	public int dig_level{set{ _dig_level = value;}get{ return _dig_level;}}
+
+	// 钓鱼等级
+	private int _fish_level;
+	public int fish_level{set{ _fish_level = value;}get{ return _fish_level;}}
+
+
+	//制造相关
+
+	//制陶等级
+	private int _mk_china_lv;
+	public int mk_china_lv{set{ _mk_china_lv = value;}get{ return _mk_china_lv;}}
+
+	//建造等级
+	private int _mk_build_lv;
+	public int mk_build_lv{set{ _mk_build_lv = value;}get{ return _mk_build_lv;}}
+
+	//建造等级
+	private int _mk_weapon_lv;
+	public int mk_weapon_lv{set{ _mk_weapon_lv = value;}get{ return _mk_weapon_lv;}}
+
+
+
+	// 装备
+	public int p_head_id;
+	public Equip p_head;
+
+	public int p_body_id;
+	public Equip p_body;
+
+	public int p_foot_id;
+	public Equip p_foot;
+
+	public int p_trousers_id;
+	public Equip p_trousers;
+
+
+	//背包
+	//探索背包
+	public List<Goods> devloper_bag = new List<Goods>();
+
+	//补给背包
+	public List<Goods> supply_bag = new List<Goods>();
+
+
+	//加成道具 4种
+
+
+}
